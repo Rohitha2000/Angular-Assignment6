a53d71de2015f9ccfda86f41d8cd1b9dace97a6a
Rohitha Gourabathuni <rohithag@valuemomentum.biz>
2022-10-31 12:09:59 +0530

    assignment6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment6';
+  @ViewChild('f') subscriptionForm: NgForm;
+  defaultSubscription = 'advanced';
+  subscription = {
+    email: '',
+    subscription: '',
+    password: '',
+  };
+  submitted = false;
+
+  onSubmit() {
+    this.submitted = true;
+    this.subscription.email = this.subscriptionForm.value.email;
+    this.subscription.subscription = this.subscriptionForm.value.subscription;
+    this.subscription.password = this.subscriptionForm.value.password;
+    console.log(this.subscriptionForm.value);
+    this.subscriptionForm.reset();
+    this.subscriptionForm.form.patchValue({
+      subscription: this.defaultSubscription,
+    });
+  }
 }
